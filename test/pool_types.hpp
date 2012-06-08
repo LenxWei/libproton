f5b2461420488215855596ed9bcedcb8a6c4a501
Lenx Tao Wei <lenx.wei@gmail.com>
2012-06-08 16:03:58 -0700

    add test/pool_types.hpp

---
+++ test/pool_types.hpp
@@ -0,0 +1,35 @@
+#ifndef HANDY_TMP_TYPES_HEADER
+#define HANDY_TMP_TYPES_HEADER
+
+#include <string>
+#include <proton/pool.hpp>
+
+namespace proton{
+
+typedef std::basic_string<char, std::string::traits_type, 
+            meta_allocator<char, tmp_pool> > tstring;
+
+template<typename string1, typename A>
+    string1 to_( const std::basic_string<char, std::string::traits_type, A>& s )
+    {
+        return string1(s.c_str(), s.size());
+    }
+
+#define tvector(T) std::vector<T, meta_allocator<T, tmp_pool> > 
+#define tdeque(T) std::deque<T, meta_allocator<T, tmp_pool> >
+#define tlist(T) std::list<T, meta_allocator<T, tmp_pool> >
+#define tmap(K,T) std::map<K, T, std::less<K >, meta_allocator<std::pair<const K, T >, tmp_pool> >
+#define tset(K) std::set<K, std::less<K >, meta_allocator<K, tmp_pool> >
+
+typedef std::basic_string<char, std::string::traits_type, 
+            meta_allocator<char, per_pool> > estring;
+
+#define evector(T) std::vector<T, meta_allocator<T, per_pool> > 
+#define edeque(T) std::deque<T, meta_allocator<T, per_pool> >
+#define elist(T) std::list<T, meta_allocator<T, per_pool> >
+#define emap(K,T) std::map<K, T, std::less<K >, meta_allocator<std::pair<const K, T >, per_pool> >
+#define eset(K) std::set<K, std::less<K >, meta_allocator<K, per_pool> >
+
+};
+
+#endif //HANDY_TMP_TYPES_HEADER
