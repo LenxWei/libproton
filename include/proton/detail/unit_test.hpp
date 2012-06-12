bdc71b4fe4cc691e3b8bc035f2c983e606d6f93a
Lenx Tao Wei <lenx.wei@gmail.com>
2012-06-12 15:56:12 -0700

    add remark for ut

--- include/proton/detail/unit_test.hpp
+++ include/proton/detail/unit_test.hpp
@@ -12,6 +12,10 @@ namespace detail{
 //@return 0 - success
 typedef int (*unittest_t)();
 
+/** a lite unit test framework.
+ * @param ut unit tests need to be tested.
+ * @return 0: succes, other: failure
+ */
 int unittest_run(std::vector<unittest_t>& ut);
 
 }}
