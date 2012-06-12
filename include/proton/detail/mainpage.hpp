cbb56bbbcfcb4614963c52fc0b55b484e1bb3f0b
Lenx Tao Wei <lenx.wei@gmail.com>
2012-06-12 15:06:56 -0700

    add doc directory for dox

--- include/proton/detail/mainpage.hpp
+++ include/proton/detail/mainpage.hpp
@@ -3,13 +3,13 @@
 * @authors Lenx Tao Wei (lenx.wei at gmail.com)
 *
 * @section intro Introduction
-* a library to make C++ as convenient as Python :)
+* a library to make C++ as convenient as Python :) <br>
 * The main git repository is at https://github.com/LenxWei/libproton.
 *
 * <hr>
+* @todo add proton containers for stl containers
 * @todo add examples
 * @todo add tutorial
-* @todo add proton containers for stl containers
 * @todo add threading support at 1.4
 * @todo add gc support at 1.6
 *
