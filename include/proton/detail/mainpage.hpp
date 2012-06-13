d68e88828350a6d1993118e3e8645c066ed6feca
Lenx Tao Wei <lenx.wei@gmail.com>
2012-06-13 01:48:04 -0700

    add my url

--- include/proton/detail/mainpage.hpp
+++ include/proton/detail/mainpage.hpp
@@ -1,15 +1,19 @@
 /** @mainpage C++11 Proton Library
 *
-* @authors Lenx Tao Wei (lenx.wei at gmail.com)
+* @authors <a href="http://lenx.100871.net">Lenx Tao Wei</a> (lenx.wei at gmail.com)
 *
 * @section intro Introduction
 * Proton is a library to make C++11 as convenient as Python :) <br>
 * The main git repository is at https://github.com/LenxWei/libproton.
 *
 * @section install Install
-* Download site: http://code.google.com/p/libproton/downloads/list .<br>
+* Download site:
+* - http://code.google.com/p/libproton/downloads/list
+* - https://github.com/LenxWei/libproton/downloads
+*
 * Need a compiler supporting c++11, such as gcc 4.6 or higher.<br>
-* Install <a href="http://www.boost.org/">Boost C++ Library</a> 1.48 or higher first.<br>
+* Install <a href="http://www.boost.org/">Boost C++ Library</a> 1.48 or higher first.
+*
 * After that, just standard "./configure; make; make check; make install"
 *
 * @section modules Modules
