c459e1d74ccfddb6351961be3967712f0cf916f2
Lenx Tao Wei <lenx.wei@gmail.com>
2012-06-17 19:42:59 -0700

    complete vector_ for the first stage

--- examples/vector.cpp
+++ examples/vector.cpp
@@ -10,17 +10,57 @@ int main()
 {
     cout << ">>> vector_ examples :" << endl;
 
-    vector_<int> s={1,2,3};
-    vector_<int> t={2,1};
+    vector_<string> s={"a","b","c"};
+    vector_<string> t={"b","a"};
 
-    s.append(2);
+    cout << "last item is :" << s[-1] << endl;
+    PROTON_THROW_IF(s[-1]!="c", "get item err");
 
-    cout << "There is " << s.count(2) << " of two in s." << endl;
-    PROTON_THROW_IF(s.count(2)!=2, "count err");
+    vector_<string> slice=s(1); // s[1:]
+    PROTON_THROW_IF(slice[-1]!="c", "slice to the end err");
+
+    slice=s(1,-1); // s[1:-1]
+    PROTON_THROW_IF(len(slice)!=1, "slice in the middle err");
+
+    slice=s(0,3,2); // s[0:3:2]
+    PROTON_THROW_IF(len(slice)!=2, "slice with step err");
+
+    slice=slice*2;
+    PROTON_THROW_IF(len(slice)!=4, "slice with step err");
+
+    slice=2*slice;
+    PROTON_THROW_IF(len(slice)!=8, "slice with step err");
+
+    s.append("b");
+
+    cout << "There is " << s.count("b") << " of b in s." << endl;
+    PROTON_THROW_IF(s.count("b")!=2, "count err");
 
     s.extend(t);
-    PROTON_THROW_IF(s.count(2)!=3, "count err");
+    PROTON_THROW_IF(s.count("b")!=3, "extend err");
 
+    int i=s.index("c");
+    PROTON_THROW_IF(i!=2, "index err");
+
+    s.insert(-1,"x");
+    PROTON_THROW_IF(s.index("x")!=long(s.size()-2), "index err");
+
+    string k=s.pop();
+    k=s.pop();
+    PROTON_THROW_IF(k!="x", "pop err");
+
+    size_t n=s.count("b");
+    s.remove("b");
+    PROTON_THROW_IF(s.count("b")!=n-1, "remove err");
+
+    string r=s[-2];
+    s.reverse();
+    cout << "reversed vector is :" << s << endl;
+    PROTON_THROW_IF(s[1]!=r, "remove err");
+
+    s.sort();
+    cout << "sorted vector is :" << s << endl;
+    PROTON_THROW_IF(s[0]!="a", "remove err");
     return 0;
 }
 
--- include/proton/vector.hpp
+++ include/proton/vector.hpp
@@ -13,23 +13,13 @@
 #include <initializer_list>
 #include <algorithm>
 #include <proton/pool.hpp>
+#include <stdexcept>
 
 namespace proton{
 
 /** @addtogroup vector_
  * @{
  */
-
-/** check if x has val.
- * @param x the container
- * @param val the value
- * @return true: has, false: not
- */
-template <typename T, typename allocT, typename V >
-bool has(const std::vector<T,allocT>& x, V&& val)
-{
-    return std::find(x.begin(), x.end(), val)!=x.end();
-}
 
 /** add an item in streaming style.
  * @param x the vector to be added
@@ -38,6 +28,18 @@ bool has(const std::vector<T,allocT>& x, V&& val)
  */
 template <typename T, typename A, typename V>
 std::vector<T,A>& operator<<(std::vector<T,A>& x, V&& val)
+{
+    x.push_back(val);
+    return x;
+}
+
+/** add an item in streaming style.
+ * @param x the vector to be added
+ * @param val the new item
+ * @return the new x
+ */
+template <typename T, typename A, typename V>
+std::vector<T,A>& operator<<(std::vector<T,A>& x, const V& val)
 {
     x.push_back(val);
     return x;
@@ -78,7 +80,7 @@ std::ostream& operator<<(std::ostream& s, std::vector<T,A>& x)
     return s;
 }
 
-/** sort a vector
+/* sort a vector
  * @param x the vector to be sorted
  */
 template <typename T, typename A>
@@ -87,7 +89,7 @@ void sort(std::vector<T,A>& x)
     std::sort(x.begin(), x.end());
 }
 
-/** get an item like python.
+/* get an item like python.
  * @param x the sequence
  * @param i the index
  * @return x[i]
@@ -103,7 +105,7 @@ T& get(std::vector<T,A>& x, long i)
     return x[i];
 }
 
-/** get a const item like python.
+/* get a const item like python.
  * @param x the sequence
  * @param i the index
  * @return x[i]
@@ -119,7 +121,7 @@ const T& get(const std::vector<T,A>& x, long i)
     return x[i];
 }
 
-/** get a slice like python.
+/* get a slice like python.
  * @param x the sequence
  * @param first the start
  * @return x[first:]
@@ -141,7 +143,7 @@ std::vector<T,A> sub(const std::vector<T,A>& x, long first)
     return r;
 }
 
-/** get a slice like python.
+/* get a slice like python.
  * @param x the sequence
  * @param first the start
  * @param last  the end
@@ -174,10 +176,44 @@ std::vector<T,A> sub(const std::vector<T,A>& x, long first, long last)
 /** a vector extension implementing python's list-like interfaces.
  */
 template <typename T, typename A=smart_allocator<T> >
-struct vector_ : public std::vector<T,A>{
-public:
+class vector_ : public std::vector<T,A>{
+    public:
     typedef std::vector<T,A> baseT;
+    typedef typename baseT::difference_type offset_t;
 
+protected:
+    offset_t __offset(offset_t i)const
+    {
+        if(i<0)
+            i+=this->size();
+        return i;
+    }
+
+    offset_t offset(offset_t i)const
+    {
+        i=__offset(i);
+        PROTON_THROW_IF(i<0 || (size_t)i>=this->size(), "out of range, offset is " << i
+                         << " while size is " << this->size() );
+        return i;
+    }
+
+    void fix_range(offset_t& begin, offset_t& end)const
+    {
+        offset_t size=(offset_t)this->size();
+        begin=__offset(begin);
+        end=__offset(end);
+        if(begin>=size || end<=0 || end<=begin){
+            begin=0;
+            end=0;
+            return;
+        }
+        if(begin<0)
+            begin=0;
+        if(end>=size)
+            end=size;
+    }
+
+public:
     /** forwarding ctor.
      */
     template<typename ...argT> vector_(argT&& ...a):baseT(a...)
@@ -187,6 +223,57 @@ public:
      */
     vector_(std::initializer_list<T> a):baseT(a)
     {}
+
+    /** cast to std::vector<>&.
+     */
+    operator baseT&()
+    {
+        return reinterpret_cast<baseT&>(*this);
+    }
+
+    /** [i] in python
+     */
+    T& operator[](offset_t i)
+    {
+        return *(this->begin()+offset(i));
+    }
+
+    /** [i] in python
+     */
+    const T& operator[](offset_t i)const
+    {
+        return *(this->begin()+offset(i));
+    }
+
+    /** slice of [i:]
+     */
+    vector_ operator()(offset_t i)const
+    {
+        auto begin=this->begin();
+        return vector_(begin+offset(i),this->end());
+    }
+
+    /** slice of [i:j]
+     */
+    vector_ operator()(offset_t i, offset_t j)const
+    {
+        auto begin=this->begin();
+        fix_range(i,j);
+        return vector_(begin+i,begin+j);
+    }
+
+    /** slice of [i:j:k]
+     */
+    vector_ operator()(offset_t i, offset_t j, size_t k)const
+    {
+        fix_range(i,j);
+        vector_ r;
+        r.reserve((j-i)/k+1);
+        auto it=this->begin()+i;
+        for(offset_t n=i; n<j; n+=k,it+=k)
+            r.push_back(*it);
+        return r;
+    }
 
     /** append an item at the end.
      */
@@ -207,19 +294,182 @@ public:
         return std::count(this->begin(), this->end(), x);
     }
 
+    /** delete the i-th item
+     */
+    void del(offset_t i)
+    {
+        this->erase(this->begin()+offset(i));
+    }
+
+    /** delete from i-th to the j-th items
+     */
+    void del(offset_t i, offset_t j)
+    {
+        fix_range(i,j);
+        auto begin=this->begin();
+        this->erase(begin+i, begin+j);
+    }
+
+    /** delete from i-th item to the end
+     */
+    void del_to_end(offset_t i)
+    {
+        this->erase(this->begin()+offset(i), this->end());
+    }
+
     /** append items from a sequence.
-     * [TODO] support general range concept.
      */
     template<typename seqT>void extend(const seqT& x)
     {
-        this->reserve(this->size()+x.size()); // [TODO] use len() to replace size()?
-        std::copy(x.begin(), x.end(), std::back_inserter(*this));
+        for(auto& i:x){
+            this->push_back(i);
+        }
     }
+
+    template<typename seqT>void extend(seqT&& x)
+    {
+        for(auto&& i:x){
+            this->push_back(i);
+        }
+    }
+
+    /** index of the first occurence of a value.
+     * @param val the value.
+     * @throw std::invalid_argument if there is no such a value.
+     */
+    offset_t index(const T& val)const
+    {
+        auto begin=this->begin(), end=this->end();
+        auto it=std::find(begin, end, val);
+        if(it==end)
+            throw std::invalid_argument("The given value doesn't exist in this sequence.");
+        return it-begin;
+    }
+
+    /** insert value at offset i.
+     * @param i the offset, like python, -1 means the last position.
+     * @param val the value.
+     * @throw proton::err if i is bad.
+     */
+    void insert(offset_t i, const T& val)
+    {
+        auto it=this->begin()+ offset(i);
+        baseT::insert(it, val);
+    }
+
+    void insert(offset_t i, T&& val)
+    {
+        auto it=this->begin()+offset(i);
+        baseT::insert(it, val);
+    }
+
+    /** pop an item from the sequence.
+     * @param i the index of the itme, default is -1, the last item.
+     * @return the value at offset i.
+     * @throw proton::err if i is bad.
+     */
+    T pop(offset_t i=-1)
+    {
+        auto it=this->begin()+offset(i);
+        T r=*it;
+        this->erase(it);
+        return r;
+    }
+
+    /** remove the first occurence of a value.
+     * @param val the value.
+     * @throw std::invalid_argument if there is no such a value.
+     */
+    void remove(const T& val)
+    {
+        auto begin=this->begin(), end=this->end();
+        auto it=std::find(begin, end, val);
+        if(it==end)
+            throw std::invalid_argument("The given value doesn't exist in this sequence.");
+        this->erase(it);
+    }
+
+    /** reverses the items in place.
+     */
+    void reverse()
+    {
+        std::reverse(this->begin(), this->end());
+    }
+
+    /** sort the items in place.
+     */
+    void sort()
+    {
+        std::sort(this->begin(), this->end());
+    }
+
+    /** sort the items in place using a customized cmp.
+     */
+    template<class cmpT>void sort(cmpT cmp)
+    {
+        std::sort(this->begin(), this->end(),cmp);
+    }
+
 };
 
 /**
  * @example vector.cpp
  */
+
+/** vector_ * n
+ */
+template<typename T, typename A>
+vector_<T,A> operator*(const std::vector<T,A>& s, size_t n)
+{
+    vector_<T,A> r;
+    r.reserve(s.size()*n);
+    for(size_t i=0; i<n; i++)
+        r.extend(s);
+    return r;
+}
+
+/** n * vector_
+ */
+template<typename T, typename A>
+vector_<T,A> operator*(size_t n, const std::vector<T,A>& s)
+{
+    return s*n;
+}
+
+/** cast to proton::vector_<>& from std::vector<>&.
+ */
+template<typename T, typename A>
+vector_<T,A>& cast_(std::vector<T,A>& x)
+{
+    return reinterpret_cast<vector_<T,A>&>(x);
+}
+
+/** cast to proton::vector_<>& from std::vector<>&.
+ */
+template<typename T, typename A>
+const vector_<T,A>& cast_(const std::vector<T,A>& x)
+{
+    return reinterpret_cast<const vector_<T,A>&>(x);
+}
+
+/** check if x has val.
+ * @param x the container
+ * @param val the value
+ * @return true: has, false: not
+ */
+template <typename T, typename allocT, typename V >
+bool has(const std::vector<T,allocT>& x, const V& val)
+{
+    return std::find(x.begin(), x.end(), val)!=x.end();
+}
+
+/** get length of vector.
+ */
+template<typename T, typename A>
+size_t len(const std::vector<T,A>& v)
+{
+    return v.size();
+}
 
 /**
  * @}
