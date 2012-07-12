ce5a30e8ce06d8ff1b3867b1ae4fa91d04ef4bbb
Lenx Tao Wei <lenx.wei@gmail.com>
2012-07-12 04:30:37 -0700

    add io.hpp

---
+++ include/proton/io.hpp
@@ -0,0 +1,27 @@
+#ifndef PROTON_IO_HEADER
+#define PROTON_IO_HEADER
+
+
+/** @file io.hpp
+ *  @brief io support.
+ */
+
+#include <iostream>
+#include <initializer_list>
+#include <algorithm>
+#include <stdexcept>
+#include <limits>
+
+#include <proton/base.hpp>
+
+namespace proton{
+
+typedef ref_<std::fstream> File;
+
+/**
+ * @example io.cpp
+ * @}
+ */
+}
+
+#endif // PROTON_IO_HEADER
