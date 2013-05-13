8c854f1d7a3388e53c0174ea45731f1a6821c099
Lenx Wei <lenx.wei@gmail.com>
2013-05-12 21:44:41 -0700

    better sub of tuple

--- include/proton/tuple.hpp
+++ include/proton/tuple.hpp
@@ -21,31 +21,6 @@ namespace proton{
 
 namespace detail{
 
-// helper function to print a tuple of any size
-template<typename T, std::size_t I>
-struct output_tuple {
-    static void output(std::ostream& s, T&& t)
-    {
-        output_tuple<T, I-1>::output(s,t);
-        s << ", " << std::get<I-1>(t);
-    }
-};
-
-template<typename T>
-struct output_tuple<T, 1> {
-    static void output(std::ostream& s, T&& t)
-    {
-        s << std::get<0>(t);
-    }
-};
-
-template<typename T>
-struct output_tuple<T, 0> {
-    static void output(std::ostream& s, T&& t)
-    {
-    }
-};
-
 constexpr long fix_index(long i, long size)
 {
     return (i>size)?
@@ -108,35 +83,6 @@ constexpr long fix_size(long begin, long end, long size)
 				fix_index(end,size)-fix_index(begin,size);
 }
 
-template<typename T, size_t begin, size_t size>
-struct sub{
-private:
-	static_assert(begin < std::tuple_size<T>::value, "out of range");
-
-	std::tuple<typename std::tuple_element<begin, T>::type> *p;
-	typedef typename sub<T, begin+1,
-						(begin+size > std::tuple_size<T>::value ?
-							(std::tuple_size<T>::value-begin-1)
-							: (size-1))
-						>::type next_types;
-	next_types* q;
-
-public:
-	typedef decltype(std::tuple_cat(*p,*q)) type;
-};
-
-template<typename T, size_t begin>
-struct sub<T, begin, 0>{
-	typedef std::tuple<> type;
-};
-
-template<typename T, size_t begin>
-struct sub<T,begin,1>{
-	static_assert(begin < std::tuple_size<T>::value, "out of range");
-
-	typedef std::tuple<typename std::tuple_element<begin,T>::type > type;
-};
-
 template<typename ...T>
 struct len_t<std::tuple<T...> >{
     static size_t result(const std::tuple<T...>& x)
@@ -154,12 +100,82 @@ public:
 	static constexpr size_t value=sizeof...(T);
 };
 
-template<typename T, long begin, long end>
-struct sub_type{
-public:
-	typedef typename sub<T, fix_index(begin, tuple_size<T>::value),
-			fix_size(begin, end, tuple_size<T>::value)>::type type;
+////////////////////
+// build index_tuple<start, ... start+size-1>
+////////////////////
+
+template<long... _Indexes>
+struct index_tuple;
+
+template<>
+struct index_tuple<>
+{};
+
+template<long start, long... _Indexes>
+struct index_tuple<start, _Indexes...>
+{
+	typedef index_tuple<start, _Indexes..., start+1+sizeof...(_Indexes)> next;
 };
+
+// Builds an _Index_tuple<0, 1, 2, ..., _Num-1>.
+template<long begin, long size>
+struct build_index_tuple
+{
+	typedef typename build_index_tuple<begin, size - 1>::type::next type;
+};
+
+template<long start>
+struct build_index_tuple<start, 1>
+{
+	typedef index_tuple<start> type;
+};
+
+template<long start>
+struct build_index_tuple<start, 0>
+{
+	typedef index_tuple<> type;
+};
+///////////////////////
+
+///////////////////////
+// get the sub type
+///////////////////////
+template<long, long, typename, typename>
+struct make_tuple_impl;
+
+template<long start, long size, typename T, typename... _Tp>
+struct make_tuple_impl<start, size, T, std::tuple<_Tp...> >
+{
+	typedef typename make_tuple_impl<start + 1, size-1,
+				T, std::tuple<_Tp..., typename std::tuple_element<start, T>::type>
+				>::type
+		type;
+};
+
+template<long start, typename T, typename... _Tp>
+struct make_tuple_impl<start, 0, T, std::tuple<_Tp...>>
+{
+	typedef std::tuple<_Tp...> type;
+};
+
+template<typename T, long start, long size>
+struct sub_tuple_type{
+	typedef typename make_tuple_impl<start, size, T, std::tuple<> >::type type;
+};
+
+//////////////////////
+
+template<typename retT, typename tupT, typename I>
+struct sub_tuple;
+
+template<typename retT, typename tupT, long... i>
+struct sub_tuple<retT, tupT, index_tuple<i...> >{
+	static retT sub(const tupT& t)
+	{
+		return retT(std::get<i>(t)...);
+	}
+};
+//////////////////////
 
 } // ns detail
 
@@ -177,8 +193,49 @@ typename detail::at_index<index,T...>::type
 	return std::get<detail::get_index(index,sizeof...(T))>(x);
 }
 
+namespace detail{
+// helper function to print a tuple of any size
+template<typename T, long I>
+struct output_tuple {
+    static void output(std::ostream& s, const T& t)
+    {
+        s << at<-I>(t) << ", ";
+        output_tuple<T, I-1>::output(s,t);
+    }
+};
+
+template<typename T>
+struct output_tuple<T, 1> {
+    static void output(std::ostream& s, const T& t)
+    {
+        s << at<-1>(t);
+    }
+};
+
+template<typename T>
+struct output_tuple<T, 0> {
+    static void output(std::ostream& s, const T& t)
+    {
+    }
+};
+
+} // ns detail
+
 /** get a slice of tuple x[begin:end] in python
  */
+
+template<long begin, long end, typename T>
+typename detail::sub_tuple_type<T, detail::fix_index(begin, detail::tuple_size<T>::value),
+		detail::fix_size(begin, end, detail::tuple_size<T>::value)>::type sub(const T& t)
+{
+	typedef typename detail::sub_tuple_type<T, detail::fix_index(begin, detail::tuple_size<T>::value),
+			detail::fix_size(begin, end, detail::tuple_size<T>::value)>::type retT;
+	return detail::sub_tuple<retT, T,
+			typename detail::build_index_tuple<detail::fix_index(begin, detail::tuple_size<T>::value),
+					detail::fix_size(begin, end, detail::tuple_size<T>::value)>::type
+			>::sub(t);
+}
+/*
 template<long begin, long end=std::numeric_limits<long>::max(), typename ...T>
 typename detail::sub_type<std::tuple<T...>, begin, end>::type
 	sub(const std::tuple<T...>& x)
@@ -194,6 +251,7 @@ typename detail::sub_type<std::tuple<T...>, begin, end>::type
 	#endif
 #endif
 }
+*/
 
 /** general output for tuple.
  * @param s the output stream
