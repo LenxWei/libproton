9d2d2e8a468852008ded93e7e499c9d411d1e3c9
Tao Wei <tao.wei@twei1-mbp.fireeye.com>
2015-01-15 14:44:12 -0800

    fix construct for g++-4.9

--- include/proton/pool.hpp
+++ include/proton/pool.hpp
@@ -437,7 +437,7 @@ public:
     template<class U, class... Args>
     static void construct(U* p, Args&&... val)
     {
-        new (p) U(val...);
+        ::new (p) U(std::forward<Args>(val)...);
     }
 
 /*
